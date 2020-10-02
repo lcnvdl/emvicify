111a44ea6854be271a23dc65967f79cef7df4216
Lcnvdl <programando.soft@gmail.com>
2020-10-02 09:55:49 -0300

    Bugfix

--- src/routers/base/abstract-router.js
+++ src/routers/base/abstract-router.js
@@ -1,6 +1,15 @@
 class AbstractRouter {
     constructor({ settings, controllers, middlewares, routerSettings }) {
-        this.baseUrl = "/";
+        this.controllers = controllers;
+        this.middlewares = middlewares;
+        this.routerSettings = routerSettings;
+
+        if (settings && settings.baseUrl) {
+            this.baseUrl = settings.baseUrl;
+        }
+        else {
+            this.baseUrl = "/";
+        }
     }
 
     registerActions() {

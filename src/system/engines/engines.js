9b213be6c611614467e3b720c1e2d8a312203329
Lcnvdl <programando.soft@gmail.com>
2020-10-05 20:28:22 -0300

    Engine

--- src/system/engines/engines.js
+++ src/system/engines/engines.js
@@ -1,7 +1,9 @@
-const ExpressEngine = require("./express-engine");
 const BaseEngine = require("../base/base-engine");
+const ExpressEngine = require("./express-engine");
+const RabbitMQEngine = require("./rabbitmq-engine");
 
 module.exports = {
     ExpressEngine,
-    BaseEngine
+    BaseEngine,
+    RabbitMQEngine
 };

7caab30641d3b67d0aeeebcba409ca33d9abc009
Lcnvdl <programando.soft@gmail.com>
2020-11-24 11:15:24 -0300

    Mfy

--- src/system/misc.js
+++ src/system/misc.js
@@ -1,4 +1,12 @@
 function getPluginMemberName(file) {
+  if (file.includes("/")) {
+    file = file.substr(file.lastIndexOf("/") + 1);
+  }
+
+  if (file.includes("\\")) {
+    file = file.substr(file.lastIndexOf("\\") + 1);
+  }
+
   return file
     .split(".")
     .slice(0, file.split(".").length - 1)
